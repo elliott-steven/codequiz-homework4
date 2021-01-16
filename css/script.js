438f7b04e33e8d7621c86685d624f15761e19dcf
Steven Elliott <steven.elliott@live.com>
2021-01-16 13:21:26 -0500

    Starting on some javascript

--- css/script.js
+++ css/script.js
@@ -11,13 +11,6 @@ var storedScores;
 var scoreList = [];
 
 
-var correctSound = new Audio("assets/audios/correct.wav");
-
-var incorrectSound = new Audio("assets/audios/incorrect.wav");
-
-
-
-
 function setTime() {
     var timerInterval = setInterval(function () {
         secondsLeft--;
